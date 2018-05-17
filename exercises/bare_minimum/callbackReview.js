1207faa75047ffa22bd2d30097ab30fbcd30f8d2
kiannaquach <kiannaquach.kq@gmail.com>
2018-05-17 12:29:02 -0700

    working on bare minimum

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,14 +6,22 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, content) {
+    if (err) callback(err);
+    // console.log()
+    callback(err, content.split('\n')[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
-};
+var getStatusCode = function (url, callback) {
+  request(url, function(err, response, body) {
+    if (err) callback(err);
+    // console.log(response.statusCode)
+    if (response) callback(err, response.statusCode);
+  })
+}
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,16 +10,30 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, 'utf8', function(err, res) {
+        if (err) reject(err);
+        if(res) resolve(res.split('\n')[0]);
+        
+    })
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
-};
+  return new Promise(function(resolve, reject) {
+    request(url, function(err, res, body) {
+      if (err) reject(err);
+      if (res) resolve(res.statusCode);
+    })
+  })
+}; 
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
   getStatusCodeAsync: getStatusCodeAsync,
   pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
 };
+
+
+  
